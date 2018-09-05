ec455d4fd7ccf3fcb58526140c53df91bf5f5613
Zhongjie He <eric6356@gmail.com>
2018-09-05 16:39:30 -0400

    init and accept offer from console

--- deploy.js
+++ deploy.js
@@ -2,6 +2,7 @@
 const fs = require('fs')
 const Web3 = require('web3')
 const web3 = new Web3('http://127.0.0.1:8545')
+const cTable = require('console.table')
 
 const TokenFactoryABI = JSON.parse(fs.readFileSync('./build/contracts_EIP20Factory_sol_EIP20Factory.abi'))
 const TokenFactoryData = fs.readFileSync('./build/contracts_EIP20Factory_sol_EIP20Factory.bin')
@@ -12,15 +13,39 @@ const ExchangeData = fs.readFileSync('./build/contracts_Exchange_sol_Exchange.bi
 
 let coinbase, TokenFactoryContract, tokenFactory, ExchangeContract, exchange, accounts
 let tokens = {}
-const getToken = address => {
-    const key = Object.keys(tokens).filter(key => tokens[key].options.address === address)[0]
-    return tokens[key]
-}
+const getTokenSymbol = address => Object.keys(tokens).filter(key => tokens[key].options.address === address)[0]
+const getToken = address => tokens[getTokenSymbol(address)]
 
 const deployed = {
-    factory: null,
-    tokens: [],
-    exchange: null
+    factory: { abi: TokenFactoryABI, data: TokenFactoryData, address: null },
+    tokens: { abi: TokenABI, data: TokenData, address: [] },
+    exchange: { abi: ExchangeABI, data: ExchangeData, address: null }
+}
+
+const _readTokenBalance = ({ symbol, balance }) => {
+    const { account } = balance
+    return tokens[symbol].methods.balances(account).call()
+        .then(b => {
+            balance[symbol] = b
+        })
+}
+
+const _readAccountBalance = ({ balance }) => {
+    const { account } = balance
+    return Promise.all([
+        _readTokenBalance({ symbol: 'TKA', account, balance }),
+        _readTokenBalance({ symbol: 'TKB', account, balance }),
+        _readTokenBalance({ symbol: 'TKC', account, balance }),
+    ])
+}
+
+const readAllBalance = () => {
+    const balances = accounts.map(account => ({ account }))
+    return Promise.resolve()
+        .then(console.group('Reading all balance...'))
+        .then(() => Promise.all(balances.map(balance => _readAccountBalance({ balance }))))
+        .then(() => console.table(balances))
+        .then(console.groupEnd)
 }
 
 const init = () => Promise.resolve()
@@ -37,13 +62,13 @@ const deployTokenFactory = () => Promise.resolve()
     .then(() => TokenFactoryContract.deploy({ data: TokenFactoryData }).send())
     .then(i => {
         tokenFactory = i
-        deployed.factory = i.options.address
+        deployed.factory.address = i.options.address
         console.log(`tokenFactory deployed at: ${i.options.address}`)
         console.groupEnd()
     })
 
 const tokenCreated = symbol => addr => {
-    deployed.tokens.push(addr)
+    deployed.tokens.address.push(addr)
     tokens[symbol] = new web3.eth.Contract(TokenABI, addr, { from: coinbase, gas: 6721975 })
     // tokens[symbol].events.Approval(console.log)
     console.log(`${symbol} deployed at: ${addr}`)
@@ -68,10 +93,10 @@ const deployExchange = () => Promise.resolve()
         ExchangeContract = new web3.eth.Contract(ExchangeABI, null, { from: coinbase, gas: 6721975 })
     })
     .then(() => console.group('Deploying exchange...'))
-    .then(() => ExchangeContract.deploy({ data: ExchangeData, arguments: [deployed.factory] }).send())
+    .then(() => ExchangeContract.deploy({ data: ExchangeData, arguments: [deployed.factory.address] }).send())
     .then(i => {
         exchange = i
-        deployed.exchange = i.options.address
+        deployed.exchange.address = i.options.address
         console.log(`exchange deployed at: ${i.options.address}`)
         console.groupEnd()
     })
@@ -79,7 +104,7 @@ const deployExchange = () => Promise.resolve()
 const _initBalance = (symbol, account, balance) => () => Promise.resolve()
     .then(() => tokens[symbol].methods.transfer(account, balance).send())
     .then(() => tokens[symbol].methods.balances(account).call())
-    .then(() => checkBalance(symbol, account))
+// .then(() => checkBalance(symbol, account))
 
 const initBalance = () => Promise.resolve()
     .then(() => console.group('Init Balance...'))
@@ -88,14 +113,43 @@ const initBalance = () => Promise.resolve()
     .then(_initBalance('TKC', accounts[3], 5000))
     .then(_initBalance('TKA', accounts[4], 100))
     .then(_initBalance('TKB', accounts[5], 200))
+    .then(readAllBalance)
     .then(console.groupEnd)
 
 const _initOffer = (account, offersSymbol, offersAmount, wantsSymbol, wantsAmount) => {
-    console.log(`${account} wants ${wantsAmount} ${wantsSymbol} for ${offersAmount} ${offersSymbol}`)
+    // console.log(`${account} wants ${wantsAmount} ${wantsSymbol} for ${offersAmount} ${offersSymbol}`)
     const offersToken = tokens[offersSymbol]
     const wantsToken = tokens[wantsSymbol]
     return offersToken.methods.approve(exchange.options.address, offersAmount).send({ from: account })
         .then(() => exchange.methods.initOffer(offersToken.options.address, offersAmount, wantsToken.options.address, wantsAmount).send({ from: account }))
+        .then(() => exchange.methods.offerCount().call())
+}
+
+const readAllOffer = () => {
+    const offers = []
+    return Promise.resolve()
+        .then(() => console.group('Reading all offer...'))
+        .then(() => exchange.methods.offerCount().call())
+        .then(c => Promise.all(Array.from(Array(parseInt(c)).keys()).map(x => {
+            const offer = { id: x + 1 }
+            offers.push(offer)
+            return _readOfferById({ offer })
+        })))
+        .then(() => console.table(offers))
+        .then(console.groupEnd)
+}
+
+const _readOfferById = ({ offer }) => {
+    const { id } = offer
+    return exchange.methods.offers(id).call()
+        .then(({ initiator, offers, offersAmount, wants, wantsAmount, acceptor }) => {
+            offer.initiator = initiator
+            offer.offers = getTokenSymbol(offers)
+            offer.offersAmount = offersAmount
+            offer.wants = getTokenSymbol(wants)
+            offer.wantsAmount = wantsAmount
+            offer.acceptor = acceptor
+        })
 }
 
 const _checkAllowance = (symbol, from, to) => {
@@ -119,17 +173,20 @@ const checkBalance = (symbol, account) => tokens[symbol].methods.balances(accoun
 const initOffer = () => Promise.resolve()
     .then(() => console.group('Init Offer...'))
     // TODO: do this from UI
-    // .then(() => _initOffer(accounts[1], 'TKA', 1000, 'TKB', 2000))
+    .then(() => _initOffer(accounts[1], 'TKA', 1000, 'TKB', 2000))
     .then(() => _initOffer(accounts[2], 'TKB', 2000, 'TKC', 3000))
     .then(() => _initOffer(accounts[3], 'TKC', 3000, 'TKA', 1000))
     .then(() => _initOffer(accounts[4], 'TKA', 100, 'TKB', 200))
+    .then(readAllOffer)
     // TODO: do this from UI
-    // .then(() => _acceptOffer(accounts[5], 4))
+    .then(() => _acceptOffer(accounts[5], 4))
+    .then(readAllBalance)
+    .then(readAllOffer)
     // .then(() => checkBalance('TKB', accounts[4]))
     // .then(() => checkBalance('TKA', accounts[5]))
     .then(console.groupEnd)
 
-web3.eth.getCoinbase()
+Promise.resolve()
     .then(init)
     .then(deployTokenFactory)
     .then(createTokens)
